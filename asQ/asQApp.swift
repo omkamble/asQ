4619a234d67f56dfc15f15ff303ee7a9492c18ee
omkamble <omk10902@gmail.com>
2023-05-07 07:55:27 +0530

    Initial Commit

---
+++ asQ/asQApp.swift
@@ -0,0 +1,17 @@
+//
+//  asQApp.swift
+//  asQ
+//
+//  Created by OM KAMBLE on 07/05/23.
+//
+
+import SwiftUI
+
+@main
+struct asQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
